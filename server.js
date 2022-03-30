0e71aeb937f13bcc11d6f129b3c4861bd624bafd
Anup <anupleo5@gmail.com>
2022-03-30 16:08:17 +0545

    asd

--- server.js
+++ server.js
@@ -5,16 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-// app.use(express.static('./dist/tournament-manager'));
+app.use(express.static('./dist/tournament-manager'));
 
-// app.get('/*', (req, res) =>
-//     res.sendFile('index.html', {root: 'dist/tournament-manager/'}),
-// );
-
-app.use(express.static(__dirname + '/dist/tournament-manager'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/tournament-manager/index.html'));});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/tournament-manager/'}),
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
